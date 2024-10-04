66d161e55865783f13ab8df396a46dd16999850b
miyajima <miyajima@csplan.jp>
2024-10-04 17:37:16 +0900

    配列で読み出し表で出力追加

--- select.php
+++ select.php
@@ -1,31 +1,42 @@
+<?php
+$dsn = 'mysql:dbname=php_db;host=localhost;charset=utf8mb4';
+$user = 'root';
+$password = 'root';
+
+try {
+    $pdo = new PDO($dsn, $user, $password);
+    $sql = 'select id, name, email FROM samurai';
+    // $pdo->query($sql);
+    $stmt = $pdo->query($sql);
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // print_r($results);
+} catch (PDOException $e) {
+    exit($e->getMessage());
+
+}
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP_SAMURAI</title>
 </head>
 <body>
-    <p>
-        <?php
-        $dsn = 'mysql:dbname=php_db;host=localhost;charset=utf8mb4';
-        $user = 'root';
-        $password = 'root';
-
-        try {
-            $pdo = new PDO($dsn, $user, $password);
-            $sql = 'select id, name FROM samurai';
-            // $pdo->query($sql);
-            $stmt = $pdo->query($sql);
-            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            print_r($results);
-        } catch (PDOException $e) {
-            exit($e->getMessage());
-
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>氏名</th>
+            <th>メールアドレス</th>
+        </tr>
+    <?php
+        foreach ($results as $result) {
+            echo "<tr><td>{$result['id']}</td><td>{$result['name']}</td><td>{$result['email']}</td></tr>";
         }
-        
-        echo 'データベースsamuraiからidと名前すべて検索しました';
         ?>
-    </p>
+    </table>
+    
+    
 </body>
 </html>
