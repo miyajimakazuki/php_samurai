1786c94a59d1923c4b141ea3922ad55efcc3a976
miyajima <miyajima@csplan.jp>
2024-10-04 09:54:55 +0900

    セレクト用ファイルを追加

---
+++ select.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP_SAMURAI</title>
+</head>
+<body>
+    <p>
+        <?php
+        $dsn = 'mysql:dbname=php_db;host=localhost;charset=utf8mb4';
+        $user = 'root';
+        $password = 'root';
+
+        try {
+            $pdo = new PDO($dsn, $user, $password);
+            $sql = 'select id, name FROM samurai';
+            $pdo->query($sql);
+        } catch (PDOException $e) {
+            exit($e->getMessage());
+
+        }
+        echo 'データベースから選びました'
+        ?>
+    </p>
+</body>
+</html>
